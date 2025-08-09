5ddacd60445fbe306fe472ff6b3149b9443997af
Dennis <itgenius1004@gmail.com>
2025-08-09 16:31:55 +0900

    ok

---
+++ agents/multi_agent/agent.py
@@ -0,0 +1,19 @@
+from google.adk.agents.llm_agent import Agent, SequentialAgent
+
+code_writer_agent = Agent(
+    model='gemini-2.0-flash',
+    name='code_writer_agent',
+    description='You are a Code Writer AI.',
+    instruction="""You are a Code Writer AI.Based on the user's \
+        request, write the initial Python code."""
+)
+code_reviewer_agent = Agent(
+    model='gemini-2.0-flash',
+    name='code_reviewer_agent',
+    description='You are a Code Reviewer AI.',
+    instruction='Review the Python code provided in the session state',
+)
+root_agent = SequentialAgent(
+    name="CodePipelineAgent",
+    sub_agents=[code_writer_agent, code_reviewer_agent],
+)
