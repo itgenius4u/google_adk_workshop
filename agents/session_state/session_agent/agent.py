df9668944504cbbf85792478ef1d60d69a51eefe
Dennis <itgenius1004@gmail.com>
2025-08-10 10:29:45 +0900

    ok

--- agents/session_state/session_agent/agent.py
+++ agents/session_state/session_agent/agent.py
@@ -3,6 +3,12 @@ from google.adk.agents.llm_agent import Agent
 root_agent = Agent(
     model='gemini-2.0-flash',
     name='root_agent',
-    description='A helpful assistant for user questions.',
-    instruction='Answer user questions to the best of your knowledge',
+    description="An agent that knows some things about the user and their posts preferences",
+    instruction="""
+        You are a helpful assistant that can respond about the user and their post preferences.
+
+    The information about the user and their post preferences is given in the state context.
+    Name: {user_name}
+    Post Preferences: {user_post_preferences}
+    """,
 )
